b6b579fd66cac80520d3b73ba060b1c574039155
Dorazar <dorazar01@gmail.com>
2025-06-29 21:53:55 +0300

    Add basic rendering

--- react-sprint3-starter/apps/mail/pages/MailIndex.jsx
+++ react-sprint3-starter/apps/mail/pages/MailIndex.jsx
@@ -1,7 +1,29 @@
-import {mailService} from '../services/mail.service.js'
+import { mailService } from '../services/mail.service.js'
 
+const { useRef, useEffect, useState, Fragment } = React
+
+const { useParams, useNavigate, Link, Outlet, useSearchParams } = ReactRouterDOM
 
 export function MailIndex() {
-    return <section className="container">Mail app</section>
-}
+  const [mails, setMails] = useState(null)
 
+  useEffect(() => {
+    loadMails()
+  }, [])
+
+  function loadMails() {
+    mailService.query().then((mails) => setMails(mails))
+  }
+
+  if (!mails || mails.length===0) return <div>Loading...</div> 
+  return (
+    <section className="container">
+      Mail app
+      <ul>{
+      mails.map(mail => (
+      <li key={mail.id}>{mail.subject}</li>) )
+      }
+      </ul>
+    </section>
+  )
+}
