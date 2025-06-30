cb6b0e96a16f52af923ad17efc6385000b99f560
Dorazar <dorazar01@gmail.com>
2025-06-30 22:57:36 +0300

    Add basic style mail app

--- apps/mail/cmps/MailFilter.jsx
+++ apps/mail/cmps/MailFilter.jsx
@@ -1,12 +1,41 @@
+
+const { useRef, useEffect, useState, Fragment } = React
+
 export function MailFilter() {
   
     
 
 
-    return (<section>
+    return (
+    
+    <Fragment>
+    
+    <section className="upper-filter">
+        
         <img className="mail-logo" src="assets\css\imgs\Gmail_icon_(2020).svg" alt="" />
         <label htmlFor="filter"></label>
         <input type="text" name="filter" placeholder="Search mails"/>
-        <button>Search</button>
-    </section>)
+    <button>Search</button>
+
+        <section >
+            <button>new mail</button>
+            <select name="" id="">
+            <option value="">All mails</option>
+            <option value="Read">Read</option>
+            <option value="Read">Unread</option>
+        </select>
+        <button>Sort by date</button>
+        <button>Sort by form</button>
+        <button>Sort by subject</button>
+        </section>
+    </section>
+    
+
+ <section className="side-filter">
+        <button>a</button>
+        <button>b</button>
+        <button>c</button>
+       </section>
+       </Fragment>
+)
 } 
