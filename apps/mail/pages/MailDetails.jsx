fef59eb4a76c0866471b3e8cf52274009cb0841c
Dorazar <dorazar01@gmail.com>
2025-07-01 19:12:12 +0300

    Show entire mail

--- apps/mail/pages/MailDetails.jsx
+++ apps/mail/pages/MailDetails.jsx
@@ -1,8 +1,39 @@
+
+const {useParams} = ReactRouterDOM
+
+const { useState,useEffect,Fragment } = React
+
+
+import { mailService } from "../services/mail.service.js"
+
 export function MailDetails() {
  
-    
+    const params = useParams()
 
+    const [mail,setMail] = useState()
+    
+    useEffect(()=>{
+        loadMail()
+    },[])
+
+
+
+    function loadMail() {
+        mailService.get(params.mailId).then(mail => setMail(mail))
+
+    }
+
+if (!mail) return <div>Loading...</div>
     return (
-        <h1>mail details</h1>
+        <Fragment>  
+            <h1>mail details</h1>
+          
+                <div>Subject:{mail.subject}</div>
+                <div>From:{mail.from}</div>
+                <div>To:{mail.to}</div>
+                <div>{mail.body}</div>
+            
+        </Fragment>
+      
     )
 }
