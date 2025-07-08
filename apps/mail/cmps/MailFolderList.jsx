f31ed97270ae3edc606a985ea366adbf75f733ec
Dorazar <dorazar01@gmail.com>
2025-07-08 21:19:49 +0300

    Add hover and focus style to left filter

--- apps/mail/cmps/MailFolderList.jsx
+++ apps/mail/cmps/MailFolderList.jsx
@@ -15,7 +15,7 @@ export function MailFolderList({ onSetFilterBy,unreadMails}) {
 
   return (
     <section className="side-filter">
-      <span className="filter-type-title" onClick={() => onSelectFolderFilter('inbox')}>
+      <button className="filter-type-title" onClick={() => onSelectFolderFilter('inbox')}>
         <span title="inbox" className="material-symbols-outlined icon-btn" >
         inbox
       </span>
@@ -23,31 +23,31 @@ export function MailFolderList({ onSetFilterBy,unreadMails}) {
         <span>
           {unreadMails === 0 ? '' : unreadMails}
         </span>
-      </span>
-         <span className="filter-type-title" onClick={() => onSelectFolderFilter('stared')}>
+      </button>
+         <button className="filter-type-title" onClick={() => onSelectFolderFilter('stared')}>
       <span className="material-symbols-outlined icon-btn">
         star
       </span>
       Starred 
-       </span>
-      <span className="filter-type-title" onClick={() => onSelectFolderFilter('sent')}>
+       </button>
+      <button className="filter-type-title" onClick={() => onSelectFolderFilter('sent')}>
       <span  className="material-symbols-outlined icon-btn" >
         send
       </span>
       Sent
-      </span >
-        <span className="filter-type-title" onClick={() => onSelectFolderFilter('draft')}>
+      </button >
+        <button className="filter-type-title" onClick={() => onSelectFolderFilter('draft')}>
       <span className="material-symbols-outlined icon-btn">
         text_snippet
       </span>
       Draft
-      </span>
-      <span className="filter-type-title" onClick={() => onSelectFolderFilter('trash')}>
+      </button>
+      <button className="filter-type-title" onClick={() => onSelectFolderFilter('trash')}>
       <span title="trash"className="material-symbols-outlined icon-btn" >
         delete
       </span>
       Trash
-      </span>
+      </button>
     
    
     </section>
