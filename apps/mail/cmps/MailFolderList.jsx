c1e18732384f04fd6df0e8561f9203bba26ec5a3
Dorazar <dorazar01@gmail.com>
2025-07-11 10:21:18 +0300

    Fix css

--- apps/mail/cmps/MailFolderList.jsx
+++ apps/mail/cmps/MailFolderList.jsx
@@ -1,11 +1,12 @@
 
-const { useParams, useNavigate,useOutletContext } = ReactRouterDOM
+const { useParams, useNavigate,useOutletContext,useSearchParams } = ReactRouterDOM
 const { useRef, useEffect, useState, Fragment } = React
 export function MailFolderList({ onSetFilterBy,unreadMails}) {
 
   const navigate=useNavigate()
+const [searchParams] = useSearchParams()
 
-
+const activeFolder = searchParams.get('folder') || 'inbox' 
 
    function onSelectFolderFilter(folderType) {
     
@@ -16,7 +17,10 @@ export function MailFolderList({ onSetFilterBy,unreadMails}) {
 
   return (
     <section className="side-filter">
-      <button className="filter-type-title" onClick={() => onSelectFolderFilter('inbox')}>
+      <button
+  className={`filter-type-title ${activeFolder === 'inbox' ? 'selected' : ''}`}
+  onClick={() => onSelectFolderFilter('inbox')}
+>
         <span title="inbox" className="material-symbols-outlined icon-btn" >
         inbox
       </span>
@@ -26,25 +30,37 @@ export function MailFolderList({ onSetFilterBy,unreadMails}) {
           {unreadMails === 0 ? '' : unreadMails}
         </span>
       </button>
-         <button className="filter-type-title"  onClick={() => onSelectFolderFilter('stared')}>
+        <button
+  className={`filter-type-title ${activeFolder === 'stared' ? 'selected' : ''}`}
+  onClick={() => onSelectFolderFilter('stared')}
+>
       <span className="material-symbols-outlined icon-btn">
         star
       </span>
       <span>Starred </span>
        </button>
-      <button className="filter-type-title" onClick={() => onSelectFolderFilter('sent')}>
+      <button
+  className={`filter-type-title ${activeFolder === 'sent' ? 'selected' : ''}`}
+  onClick={() => onSelectFolderFilter('sent')}
+>
       <span  className="material-symbols-outlined icon-btn" >
         send
       </span>
       <span>Sent</span>
       </button >
-        <button className="filter-type-title" onClick={() => onSelectFolderFilter('draft')}>
+     <button
+  className={`filter-type-title ${activeFolder === 'draft' ? 'selected' : ''}`}
+  onClick={() => onSelectFolderFilter('draft')}
+>
       <span className="material-symbols-outlined icon-btn">
         text_snippet
       </span>
       <span>Draft</span>
       </button>
-      <button className="filter-type-title" onClick={() => onSelectFolderFilter('trash')}>
+     <button
+  className={`filter-type-title ${activeFolder === 'trash' ? 'selected' : ''}`}
+  onClick={() => onSelectFolderFilter('trash')}
+>
       <span title="trash"className="material-symbols-outlined icon-btn" >
         delete
       </span>
